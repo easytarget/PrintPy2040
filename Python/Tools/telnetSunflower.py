4f657e8d437e167524010a8187c12ce3009078a2
Owen <owen@easytarget.org>
2024-02-05 17:56:57 +0100

    data loop work

--- Python/Tools/telnetSunflower.py
+++ Python/Tools/telnetSunflower.py
@@ -1,8 +1,17 @@
 from telnetlib import Telnet as telnet
 from time import sleep
+import json
+
+''' M409 keys of interest:
+    heat
+    job
+    sensors
+    ?seqs
+    state
+'''
 
 HOST = "10.0.0.30"
-password = "superKrib"
+password = "freedumb"
 
 tn = telnet(HOST)
 
@@ -12,7 +21,23 @@ tn.write(password.encode('ascii') + b"\n")
 print('sent')
 print(tn.read_until(b"Log in successful!").decode('ascii'))
 while True:
-    tn.write(b'M409 F"fnd99"\n')
-    print('M409 sent')
-    print(tn.read_until(b"ok").decode('ascii'))
+    # cmd= b'M409 F"fnd99" K"job"\n'
+    cmd = b'M409 F"vd2"\n'
+    tn.write(cmd)
+    print('SEND: '+ str(cmd))
+    response = tn.read_until(b"ok").decode('ascii').split()
+    print(response)
+    print(type(response))
+    #for key in response:
+    #    print("DATA: " + key + " = " + str(response[key]))
+    print(len(response))
+    try:
+        payload = json.load(response[0])
+    except:
+        print("????")
+        payload = []
+    for key in payload['result']:
+        print("DATA: " + key + " = " + str(payload['result'][key]))
+    else:
+        print("No Payload!")
     sleep(10)
