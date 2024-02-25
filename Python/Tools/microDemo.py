d6dce6ccb1a392b1403701573cc6e1572c25a76b
Owen <owen@easytarget.org>
2024-02-25 05:32:44 +0100

    port number fix

--- Python/Tools/microDemo.py
+++ Python/Tools/microDemo.py
@@ -1,7 +1,7 @@
 from serialOM import serialOM
 from serial import Serial
 
-rrf = Serial('/dev/ttyACM1',57600)
+rrf = Serial('/dev/ttyACM0',57600)
 OM=serialOM(rrf, {'FFF':[],'CNC':[],'Laser':[]}, quiet=True)
 print('state: ' + OM.model['state']['status'] 
      + ', up: ' + str(OM.model['state']['upTime']) + 's')
