1d136293d0118795a861afb4b9d9e45f010e6568
Owen <owen@easytarget.org>
2024-02-28 14:01:16 +0100

    arguments and comments

--- Python/Tools/printPy/printPy.py
+++ Python/Tools/printPy/printPy.py
@@ -21,10 +21,16 @@ from sys import executable
 from os import execv
 
 '''
-    serialOM.py demo
+    PrintPy is a serialOM.py demo/example.
 
     This is intended to be run on a desktop system (CPython, not microPython)
     that connects via serial or USBserial to a RRF 3.x based controller.
+
+    For requirements and arguments please refer to the  README, the only real
+    requirement is py-serial ('pip install --user pyserial' etc..)
+
+    As with serialOM itself there a comments hera about microPython use, iplease
+    ignore these since they are reminders for me to use when porting this later
 '''
 
 # local print function so we can suppress info messages.
@@ -67,12 +73,14 @@ if config.quiet:
 else:
     print(argv[0] + ' is starting at: ' + startDate + ' ' + startTime + ' (device localtime)')
 
-# Arguments, optional, #1 is update timer, the rest are serial device ports
+# Arguments, optional, #1 is update timer, #2 serial device, #3 baud.
 if len(argv) > 1:
-    config.updateTime = argv[1]
-    config.requestTimeout = config.updateTime * 0.66
+    config.updateTime = int(argv[1])
+    config.requestTimeout = int(config.updateTime * 0.66)
 if len(argv) > 2:
-    config.devices = argv[2:]
+    config.devices = [str(argv[2])]
+if len(argv) > 3:
+    config.baud = int(argv[3])
 
 # Debug Logging
 rawLog = None
@@ -115,6 +123,8 @@ if not rrf:
     # Loop looking for a serial device
     # For micropython we should stop here since no UART == a serious fail.
     restartNow('No USB/serial device found')
+else:
+    print('connected to: ' + rrf.name + ' @' + str(rrf.baudrate))
 
 # create the OM handler
 try:
