43af1af55fa7912b85e5adf720336bf10d39edb7
Owen Carter <owen.carter@gmail.com>
2024-11-29 15:46:37 +0100

    pwm heartbeat

--- microPython/heartbeatXIAO2040.py
+++ microPython/heartbeatXIAO2040.py
@@ -1,4 +1,4 @@
-from machine import Timer,Pin
+from machine import Timer, PWM
 '''
     Heartbeat (RGB LED Indicator) for the Seeedstudio XIAO RP2040
 '''
@@ -12,28 +12,28 @@ class heartbeat:
                 bright = float(0..1), intensity
                 standby = float(0..1), intensity when off
         '''
-        self.bright = bright
-        self.standby = standby
+        self.bright = int(bright * 65535)
+        self.standby = int(standby * 65535)
 
-        # RGB mini status Led, default off
-        self._rgbR = Pin(17,Pin.OUT)
-        self._rgbG = Pin(16,Pin.OUT)
-        self._rgbB = Pin(25,Pin.OUT)
-        self._rgbstate = (True,False,False)
-        self._setRGB()   # start off
+        # RGB mini status Led, default off, inverted wiring
+        self._rgbR = PWM(17, freq=5000, duty_u16=0, invert = True)
+        self._rgbG = PWM(16, freq=5000, duty_u16=0, invert = True)
+        self._rgbB = PWM(25, freq=5000, duty_u16=0, invert = True)
+        self._setRGB((0,0,0),0)     # start off
+        self._rgbstate = (0, 1, 1)  # we will 'cycle' this tuple
 
-    def _setRGB(self,state=(False,False,False)):
-        # sets onboard rgb status led (heartbeat led)
-        self._rgbR.value(not state[0])   # pins are inverted
-        self._rgbG.value(not state[1])
-        self._rgbB.value(not state[2])
+    def _setRGB(self, state, bright):
+        self._rgbR.duty_u16(state[0] * bright)
+        self._rgbG.duty_u16(state[1] * bright)
+        self._rgbB.duty_u16(state[2] * bright)
 
     def beat(self, dim=False):
         '''
             Shows comms activity using the spare RGB led on the Xiao board
-            cycling R->G->B->etc every time a request is sent
+            Two leds are illuminated at all times
+            cycling GB->BR->RG->etc every time a request is sent
         '''
-        # TODO: dimming function
-        self._setRGB(self._rgbstate)
-        # Rotate the onboard RGB heartbeat
+        bright = self.standby if dim else self.bright
+        self._setRGB(self._rgbstate, bright)
+        # Rotate the state tuple
         self._rgbstate = (self._rgbstate[2],self._rgbstate[0],self._rgbstate[1])
