085c3648b56f54003e27594e49c98977bb0e4cc3
Owen Carter <owen.carter@gmail.com>
2024-12-03 16:38:50 +0100

    fix

--- microPython/main.py
+++ microPython/main.py
@@ -1,11 +1,13 @@
-import config
+from config import config
 from time import sleep_ms
-if not config.debug == 0:
+if config.debug == 0:
     import printXIAO
 elif config.debug > 0:
+    print('Autostart: ',end='')
     for i in range(config.debug,0,-1):
         print('{}'.format(i),end='')
         sleep_ms(500)
         print('.',end='')
-    print()
+        sleep_ms(500)
+    print('now')
     import printXIAO
