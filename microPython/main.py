9f6fba76230bce0feb2d3256941f251db5cf494c
Owen Carter <owen.carter@gmail.com>
2026-08-15 18:30:27 +0200

    allow start timeout to be "None"

--- microPython/main.py
+++ microPython/main.py
@@ -5,21 +5,23 @@
 # printXIAO on device startup and hard reboots.
 
 print('PrintXIAO loader')
-from sys import exit
 from time import sleep_ms
 try:
     from config import config
 except ModuleNotFoundError:
     print('Cannot find config.py')
-    exit()
-
-if config.debug > 0:
-    print('Autostart: ',end='')
-    for i in range(config.debug,0,-1):
-        print('{}'.format(i),end='')
-        sleep_ms(500)
-        print('.',end='')
-        sleep_ms(500)
-    print('now')
-    import printXIAO
+    print('Copy "config-default.py" to "config.py" and edit as needed')
+else:
+    if config.debug is None:
+        import printXIAO
+    elif config.debug > 0:
+        print('Autostart: ',end='')
+        for i in range(config.debug,0,-1):
+            print('{}'.format(i),end='')
+            sleep_ms(500)
+            print('.',end='')
+            sleep_ms(500)
+        print('now')
+        import printXIAO
+# Otherwise, just drop out to the repl prompt
 print('Debug mode; not starting')
